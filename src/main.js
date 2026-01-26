e05f2712d347bbcc7981591c0c8955c5b4ff1b61
maximemlly <maxime.maillary@icloud.com>
2026-01-26 11:29:32 +0100

    update main.js to render game title and start button in the app

--- src/main.js
+++ src/main.js
@@ -2,9 +2,12 @@ import "./style.css";
 import { Grid } from "./models/Grid.js";
 import { BoardView } from "./views/boardView.js";
 
-const canvas = document.getElementById("board-canvas");
-
-const grid = new Grid();
-const board = new BoardView(canvas, grid, { tileSize: 56 });
-
-window.addEventListener("resize", () => board.resize());
+document.querySelector("#app").innerHTML = `
+  <div id="title">
+    <p>Welcome to</p>
+    <h1>COD'BLAST</h1>
+  </div>
+  <p id="subtitle">Current highscore: mettre valeur dynamique</p>
+  <button id="start-button">Start Game</button>
+  <p id="credits">Made with :heart: by <a href="https://github.com/devilishlyney/">Emery</a>, <a href="https://github.com/maximemlly/">Maxime</a> and <a href="https://github.com/thegesse/">Esteban</a></p>
+  `;
