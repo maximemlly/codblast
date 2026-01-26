cc3716e1c563a1329c737d5525c0d0e81c70066f
Emery <emerypakula@gmail.com>
2026-01-26 11:31:05 +0100

    homepage

--- src/main.js
+++ src/main.js
@@ -1,1 +1,13 @@
 import "./style.css";
+import { Grid } from "./models/Grid.js";
+import { BoardView } from "./views/BoardView.js";
+
+document.querySelector('#app').innerHTML = `
+  <div id="title">
+    <p>Welcome to</p>
+    <h1>COD'BLAST</h1>
+  </div>
+  <p id="subtitle">Current highscore: mettre valeur dynamique</p>
+  <button id="start-button">Start Game</button>
+  <p id="credits">Made with ❤️ by <a href="https://github.com/devilishlyney/">Emery</a>, <a href="https://github.com/maximemlly/">Maxime</a> and <a href="https://github.com/thegesse/">Esteban</a></p>
+  `
